3ef9f3329371130362c78c81cc70bdb019a2372f
yaskanchapa <yaskanchapa@yahoo.co.jp>
2021-05-10 22:50:43 +0900

    100

---
+++ 5月/0510_1.py
@@ -0,0 +1,11 @@
+# 入力17:23:57　出力23のみ
+a=input().split(':')
+print(a[1])
+# h, m, s = input().split(':')
+# print(m)
+
+# 入力hello world　出力helloworld
+a=input().split()
+print("{}{}".format(a[0],a[1]))
+# a, b = input().split()
+# print(a+b)
---
+++ 5月/0510_2.py
@@ -0,0 +1,10 @@
+# 입력:123 -123 출력 0
+a,b = input().split()
+c=int(a)+int(b) # int 안주면 이렇게 문자열 취급해서 나옴 123-123
+print(c) 
+
+#정수를 줄바꿔 입력받고 합출력
+x = input()
+y = input()
+d=float(x)+float(y)
+print(d)
